c394174877a1599cded63f1f076fceb6aff93cac
kuyesu <2019bce028@std.must.ac.ug>
2022-08-13 11:24:36 +0300

    present

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -5,12 +5,4 @@ export async function middleware(req: any) {
   if (req.nextUrl.pathname === '/') {
     return NextResponse.redirect(new URL('/overview', req.url));
   }
-  if (req.nextUrl.pathname === '/overview') {
-    const isTokenExist = !Object.keys(req.cookies).find((s) =>
-      s.includes('session-token')
-    );
-
-    if (isTokenExist)
-      return NextResponse.redirect(new URL('/api/auth/signin', req.url));
-  }
 }
